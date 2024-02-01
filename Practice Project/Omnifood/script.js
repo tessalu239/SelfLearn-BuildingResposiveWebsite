8b4c1373178ec4114d5f36608e0a186501103322
tessalu239 <minhthulu2399@gmail.com>
2024-02-01 15:52:07 -0500

    Completed

--- Practice Project/Omnifood/script.js
+++ Practice Project/Omnifood/script.js
@@ -5,7 +5,60 @@ const currentYear = new Date().getFullYear();
 year.textContent = currentYear;
 ///////////////////////////////////////////////////////////
 // Make mobile navigation bar works
+const groupMobileBtn = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+groupMobileBtn.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+});
 
+///////////////////////////////////////////////////////////
+// TO SCROLL SMOOTHLY ON ALL DEVICE
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    //scroll to the top
+    if (href === "#") {
+      window.scrollTo({
+        top: 20,
+        behavior: "smooth",
+      });
+    }
+    //scroll into each section
+    if (href !== "#" && href.startsWith("#")) {
+      const selectedLink = document.querySelector(href);
+      selectedLink.scrollIntoView({ behavior: "smooth" });
+    }
+    //close the mobil menu bar after scrolling
+    if (link.classList.contains("nav-items"))
+      header.classList.toggle("nav-open");
+  });
+});
+//////////////////////////////////////////////////////////
+// TO STICK THE NAV BAR WHILE SCROLLING
+const sectionHero = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHero);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
@@ -29,52 +82,5 @@ checkFlexGap();
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
 
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
-
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
-
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
-
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
-
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
-
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
-
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
 */
