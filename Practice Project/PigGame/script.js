e095b2a4847d2b6d48e62aab1a5251a7e3560a3a
tessalu239 <minhthulu2399@gmail.com>
2024-02-10 15:08:43 -0500

    how to play added

--- Practice Project/PigGame/script.js
+++ Practice Project/PigGame/script.js
@@ -79,3 +79,31 @@ btnHold.addEventListener('click', () => {
 });
 //New game btn functionality
 btnNew.addEventListener('click', init);
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelector('.how-to-play');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnOpenModal.addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
