1358eab4c8cd0f464b55381e4c7b30569bd0830e
tessalu239 <minhthulu2399@gmail.com>
2024-02-23 00:40:59 -0500

    home page finished

--- Practice Project/ebookWebsite/js/script.js
+++ Practice Project/ebookWebsite/js/script.js
@@ -1,1 +1,13 @@
-// Add custom JavaScript here
+const userScroll = () => {
+  const navbar = document.querySelector(".navbar");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("transparent");
+    } else {
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("transparent");
+    }
+  });
+};
+document.addEventListener("DOMContentLoaded", userScroll);
