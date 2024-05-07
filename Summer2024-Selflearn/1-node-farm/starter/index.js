e253f311ccaf607c672f84f118f335209b4f3895
mtlu2 <minhthulu2399@gmail.com>
2024-05-07 12:46:25 -0400

    card page commit

--- Summer2024-Selflearn/1-node-farm/starter/index.js
+++ Summer2024-Selflearn/1-node-farm/starter/index.js
@@ -62,10 +62,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   //OVERVIEW PAGE
-  if (pathName === "/overview" || pathName === "/") {
+  if (pathname === "/overview" || pathname === "/") {
     res.writeHead(200, { "Content-type": "text/html" });
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
@@ -75,11 +75,14 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     //PRODUCT PAGE
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT page");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //API PAGE
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
 
