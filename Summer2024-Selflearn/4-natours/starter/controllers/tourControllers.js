452d20b8c76db558573d24ec0042128f58f2d307
mtlu2 <minhthulu2399@gmail.com>
2024-06-18 16:29:05 -0400

    Filtering Updated

--- Summer2024-Selflearn/4-natours/starter/controllers/tourControllers.js
+++ Summer2024-Selflearn/4-natours/starter/controllers/tourControllers.js
@@ -7,12 +7,24 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    //BUILD QUERY
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
-    console.log(req.query, queryObj);
+    //console.log(req.query, queryObj);
 
-    const tours = await Tour.find(queryObj);
+    //ADVANCED FILTERING
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+    //returns is duration: { lte: '5' }, difficulty: 'easy' }
+    //what we want is { duration: { $lte: 5 }, difficulty: 'easy' }
+    //all cases for query gte, lt,lte,gt
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    //EXECUTE QUERY
+    const tours = await query;
     res.status(200).json({
       status: 'success',
       result: tours.length,
