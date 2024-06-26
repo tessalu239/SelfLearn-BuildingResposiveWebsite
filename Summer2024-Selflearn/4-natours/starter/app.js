51ea0d4f8b4de87f4c776919d67f4b52511b39a3
mtlu2 <minhthulu2399@gmail.com>
2024-06-26 17:49:34 -0400

    errorControllers & appError added

--- Summer2024-Selflearn/4-natours/starter/app.js
+++ Summer2024-Selflearn/4-natours/starter/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
+
+const AppError = require('./utils/appError.js');
+const globalErrorHandler = require('./controllers/errorController.js');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -42,23 +47,16 @@ app.use('/api/v1/users', userRouter);
 
 //all undefined routes
 app.all('*', (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} in this server`);
-  err.statusCode = 404;
-  err.status = 'fail';
+  // const err = new Error(`Can't find ${req.originalUrl} in this server`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
 
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} in this server`, 404));
 });
 
-//ERROR HANDLING MIDDLEWARE
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+//MIDDLEWARE FOR HANDLING ERRORS
+app.use(globalErrorHandler);
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
 module.exports = app;
 
 //Start the server
