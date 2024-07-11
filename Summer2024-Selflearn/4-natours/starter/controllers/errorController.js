fdf2087b8c35e2824f048e95c0ff8febcc18907e
mtlu2 <minhthulu2399@gmail.com>
2024-07-11 15:15:51 -0400

    IDs Error Handling Updated

--- Summer2024-Selflearn/4-natours/starter/controllers/errorController.js
+++ Summer2024-Selflearn/4-natours/starter/controllers/errorController.js
@@ -1,14 +1,20 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    error: err,
+    err: err,
     message: err.message,
     stack: err.stack,
   });
 };
 
-const sendErrorPro = (err, res) => {
-  //Operational, trusted error: send message to client
+const sendErrorProd = (err, res) => {
+  //Operational, trusted error:send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
@@ -17,24 +23,26 @@ const sendErrorPro = (err, res) => {
 
     //Programming or other unknown error: don't leak error details
   } else {
-    //1.Log error
+    //1. Log Error
     console.error('ERROR 💥', err);
 
-    //2.Send generic message
+    //2. Send generic message
     res.status(500).json({
       status: 'error',
-      message: 'Something went wrong',
+      message: 'Something went wrong!',
     });
   }
 };
-//ERROR HANDLING MIDDLEWARE
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    return sendErrorDev(err, res);
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    return sendErrorPro(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
