8980d3b04385b0903462462195892b3fc27cf8bd
mtlu2 <minhthulu2399@gmail.com>
2024-07-12 13:29:56 -0400

    userModel.js Updated

---
+++ Summer2024-Selflearn/4-natours/starter/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email!'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password!'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: 'Password confirmation is not matched',
+    },
+  },
+});
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- Summer2024-Selflearn/4-natours/starter/server.js
+++ Summer2024-Selflearn/4-natours/starter/server.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -21,8 +27,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log('💥', err.name, err.message);
   console.log('UNHANDLED REJECTION! 💥 Shutting down... ');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
