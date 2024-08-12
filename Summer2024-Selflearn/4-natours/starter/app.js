8538a6f7aed30008649c955d8821d63716ba3ff8
mtlu2 <minhthulu2399@gmail.com>
2024-08-12 12:30:29 -0400

    Adding Rate-Limiting

--- Summer2024-Selflearn/4-natours/starter/app.js
+++ Summer2024-Selflearn/4-natours/starter/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
@@ -9,11 +10,19 @@ const globalErrorHandler = require('./controllers/errorController.js');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-//1) Middleware
-app.use(express.json());
+//1) GLOBAL MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
