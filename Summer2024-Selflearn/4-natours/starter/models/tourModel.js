6729aa6debdedca33325ff7866757b4271bba523
mtlu2 <minhthulu2399@gmail.com>
2024-08-13 11:47:24 -0400

    Embedding user into tour

--- Summer2024-Selflearn/4-natours/starter/models/tourModel.js
+++ Summer2024-Selflearn/4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./../models/userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -99,6 +100,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -111,11 +113,16 @@ tourSchema.virtual('durationWeeks').get(function () {
 });
 
 // //DOCUMENT MIDDLEWARE: runs before .save() and .create() but not .insertMany()
-// tourSchema.pre('save', function (next) {
-//   this.slug = slugify(this.name, { lower: true });
-//   next();
-// });
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
 // tourSchema.post('save', function (doc, next) {
 //   console.log(doc);
 //   next();
