900df50e86ddb90b66260d695ff14c87f31ffbff
mtlu2 <minhthulu2399@gmail.com>
2024-08-13 15:47:21 -0400

    reviewController &reviewRoutes added

--- Summer2024-Selflearn/4-natours/starter/app.js
+++ Summer2024-Selflearn/4-natours/starter/app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController.js');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes.js');
 
 //1) GLOBAL MIDDLEWARE
 //Set security HTTP headers
@@ -86,6 +87,7 @@ app.use((req, res, next) => {
 //Mounting the routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 //all undefined routes
 app.all('*', (req, res, next) => {
