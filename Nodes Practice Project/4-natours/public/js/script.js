c0fa4e97c69580156b707805a21f355915cb40c3
mtlu2 <minhthulu2399@gmail.com>
2024-08-23 12:15:55 -0400

    finished tour-card

--- Nodes Practice Project/4-natours/public/js/script.js
+++ Nodes Practice Project/4-natours/public/js/script.js
@@ -3,10 +3,10 @@ function userScroll() {
 
   window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
-      navbar.classList.add('bg-primary');
+      navbar.classList.add('bg-dark');
       navbar.classList.add('navbar-sticky');
     } else {
-      navbar.classList.remove('bg-primary');
+      navbar.classList.remove('bg-dark');
       navbar.classList.remove('navbar-sticky');
     }
   });
