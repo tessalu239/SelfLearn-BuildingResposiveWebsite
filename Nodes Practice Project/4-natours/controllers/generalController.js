97926359ae55ac93514fdc3f52245bf62e821250
mtlu2 <minhthulu2399@gmail.com>
2024-09-03 16:20:09 -0400

    token added

--- Nodes Practice Project/4-natours/controllers/generalController.js
+++ Nodes Practice Project/4-natours/controllers/generalController.js
@@ -1,9 +1,40 @@
-const Tour = require('../models/tourModel');
-const User = require('../models/userModel');
-const Review = require('../models/reviewModel');
-const catchAsync = require('../utils/catchAsync');
+const User = require('./../models/userModel');
+const Review = require('./../models/reviewModel');
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 const sendEmail = require('./../utils/email');
-//const jwt = require('jsonwebtoken');
+
+// Create JWT Token
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+// Create and Send Token as Cookie and Render Page
+const createSendTokenAndRender = (user, statusCode, res, page) => {
+  const token = signToken(user._id);
+
+  // Cookie options
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  // Send cookie with JWT
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from the output
+  user.password = undefined;
+
+  // Render the page
+  res.status(statusCode).redirect(page);
+};
 
 exports.getHomePage = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
@@ -238,7 +269,7 @@ exports.signupFunction = async (req, res) => {
           msg,
         })
           .then(() => {
-            res.redirect('/');
+            createSendTokenAndRender(newUser, 201, res, '/');
           })
           .catch((err) => {
             console.log(err);
