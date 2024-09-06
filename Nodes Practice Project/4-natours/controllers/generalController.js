dab94843385601da3267373c8cc078c027d20702
mtlu2 <minhthulu2399@gmail.com>
2024-09-06 14:18:38 -0400

    error page added

--- Nodes Practice Project/4-natours/controllers/generalController.js
+++ Nodes Practice Project/4-natours/controllers/generalController.js
@@ -37,44 +37,56 @@ const createSendTokenAndRender = (user, statusCode, res, page) => {
 };
 
 exports.getHomePage = catchAsync(async (req, res, next) => {
-  const tours = await Tour.find();
-  const reviews = await Review.find();
-  if (!tours || !reviews) {
-    console.error('Error fetching');
-    res.status(500).send('Server Error');
+  try {
+    const tours = await Tour.find();
+    const reviews = await Review.find();
+    if (!tours || !reviews) {
+      console.error('Error fetching');
+      res.status(400).render('general/error');
+    }
+
+    const featuredTours = tours.filter((tour) => tour.feature);
+    const featuredReviews = reviews.filter((review) => review.featured);
+
+    res.status(200).render('general/home', {
+      featuredTours,
+      featuredReviews,
+    });
+  } catch (err) {
+    res.status(400).render('general/error');
   }
-
-  const featuredTours = tours.filter((tour) => tour.feature);
-  const featuredReviews = reviews.filter((review) => review.featured);
-
-  res.status(200).render('general/home', {
-    featuredTours,
-    featuredReviews,
-  });
 });
 
 exports.getAllTours = catchAsync(async (req, res) => {
-  const tours = await Tour.find();
+  try {
+    const tours = await Tour.find();
 
-  if (!tours) {
-    console.error('Error fetching tours:');
-    res.status(500).send('Server Error');
+    if (!tours) {
+      console.error('Error fetching tours:');
+      res.status(400).render('general/error');
+    }
+
+    res.status(200).render('general/allTours', { tours });
+  } catch (err) {
+    res.status(400).render('general/error');
   }
-
-  res.status(200).render('general/allTours', { tours });
 });
 
 exports.getTour = catchAsync(async (req, res) => {
-  const tour = await Tour.findById(req.params.id).populate({
-    path: 'reviews',
-    fields: 'review rating user',
-  });
+  try {
+    const tour = await Tour.findById(req.params.id).populate({
+      path: 'reviews',
+      fields: 'review rating user',
+    });
 
-  if (!tour) {
-    console.log('Error to fetch the request tour');
-    res.status(500).send('Server Error');
+    if (!tour) {
+      console.log('Error to fetch the request tour');
+      res.status(400).render('general/error');
+    }
+    res.status(200).render('general/tour', { tour });
+  } catch (err) {
+    res.status(400).render('general/error');
   }
-  res.status(200).render('general/tour', { tour });
 });
 exports.loginPage = (req, res) => {
   res.status(200).render('general/login', {
@@ -86,77 +98,85 @@ exports.loginPage = (req, res) => {
 };
 
 exports.loginFunction = async (req, res) => {
-  console.log(req.body);
-  const { email, password } = req.body;
+  try {
+    console.log(req.body);
+    const { email, password } = req.body;
 
-  let values = req.body;
+    let values = req.body;
 
-  let passedValidation = true;
-  let validationMsg = {};
-  let errors = [];
+    let passedValidation = true;
+    let validationMsg = {};
+    let errors = [];
 
-  //check email
-  if (typeof email !== 'string') {
-    passedValidation = false;
-    validationMsg.email = 'You must specify an email';
-  } else if (email.trim().length === 0) {
-    passedValidation = false;
-    validationMsg.email = 'Email is required';
-  }
+    //check email
+    if (typeof email !== 'string') {
+      passedValidation = false;
+      validationMsg.email = 'You must specify an email';
+    } else if (email.trim().length === 0) {
+      passedValidation = false;
+      validationMsg.email = 'Email is required';
+    }
 
-  //Check password
-  if (typeof password !== 'string') {
-    passedValidation = false;
-    validationMsg.password = 'Password is required';
-  } else if (password.trim().length === 0) {
-    passedValidation = false;
-    validationMsg.password = 'Password is required';
-  }
+    //Check password
+    if (typeof password !== 'string') {
+      passedValidation = false;
+      validationMsg.password = 'Password is required';
+    } else if (password.trim().length === 0) {
+      passedValidation = false;
+      validationMsg.password = 'Password is required';
+    }
 
-  if (passedValidation) {
-    await User.findOne({
-      email,
-    })
-      .select('+password')
-      .then(async (user) => {
-        //complete the search(successful)
-        if (user) {
-          //Found the user document
-          //compare the password
-          await user
-            .correctPassword(password, user.password)
-            .then((isMatched) => {
-              //Done comparing the password
-              if (isMatched) {
-                //password matched
-                //create a session
-                //req.session.user=user;
-                req.session.user = user;
+    if (passedValidation) {
+      await User.findOne({
+        email,
+      })
+        .select('+password')
+        .then(async (user) => {
+          //complete the search(successful)
+          if (user) {
+            //Found the user document
+            //compare the password
+            await user
+              .correctPassword(password, user.password)
+              .then((isMatched) => {
+                //Done comparing the password
+                if (isMatched) {
+                  //password matched
+                  //create a session
+                  //req.session.user=user;
+                  req.session.user = user;
 
-                createSendTokenAndRender(user, 200, res, '/');
-                console.log(user);
-              } else {
-                //password not matched
-                errors.push('The password is not matched');
-                console.log(errors[0]);
-                res.render('general/login', { values, validationMsg, errors });
-              }
-            });
-        } else {
-          //user not found
-          errors.push("Couldn't find the email in the database");
+                  createSendTokenAndRender(user, 200, res, '/');
+                  console.log(user);
+                } else {
+                  //password not matched
+                  errors.push('The password is not matched');
+                  console.log(errors[0]);
+                  res.render('general/login', {
+                    values,
+                    validationMsg,
+                    errors,
+                  });
+                }
+              });
+          } else {
+            //user not found
+            errors.push("Couldn't find the email in the database");
+            console.log(errors[0]);
+            res.render('general/login', { values, validationMsg, errors });
+          }
+        })
+        .catch((err) => {
+          //not able to query the database
+          errors.push('Not able to query the database: ' + err);
           console.log(errors[0]);
           res.render('general/login', { values, validationMsg, errors });
-        }
-      })
-      .catch((err) => {
-        //not able to query the database
-        errors.push('Not able to query the database: ' + err);
-        console.log(errors[0]);
-        res.render('general/login', { values, validationMsg, errors });
-      });
-  } else {
-    res.render('general/login', { values, validationMsg, errors });
+        });
+    } else {
+      res.render('general/login', { values, validationMsg, errors });
+    }
+  } catch (err) {
+    res.status(400).render('general/error');
   }
 };
 
@@ -191,110 +211,114 @@ exports.signupPage = (req, res) => {
 };
 
 exports.signupFunction = async (req, res) => {
-  console.log(req.body);
-  const { name, email, password, passwordConfirm } = req.body;
+  try {
+    console.log(req.body);
+    const { name, email, password, passwordConfirm } = req.body;
 
-  let values = req.body;
+    let values = req.body;
 
-  let passedValidation = true;
-  let validationMsg = {};
-  let errors = [];
-  //check first name
-  if (typeof name !== 'string') {
-    passedValidation = false;
-    validationMsg.name = 'You must specify a first name';
-  } else if (name.trim().length === 0) {
-    passedValidation = false;
-    validationMsg.name = 'First name is required';
-  }
-
-  //check email
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (typeof email !== 'string') {
-    passedValidation = false;
-    validationMsg.email = 'You must specify an email';
-  } else if (email.trim().length === 0) {
-    passedValidation = false;
-    validationMsg.email = 'Email is required';
-  } else if (!emailRegex.test(email)) {
-    passedValidation = false;
-    validationMsg.email = 'Email is invalid';
-  }
-
-  //Check password
-  if (typeof password !== 'string') {
-    passedValidation = false;
-    validationMsg.password = 'Password is required';
-  } else if (password.trim().length === 0) {
-    passedValidation = false;
-    validationMsg.password = 'Password is required';
-  } else {
-    validationMsg.password = '';
-    if (password.length < 8 || password.length > 12) {
+    let passedValidation = true;
+    let validationMsg = {};
+    let errors = [];
+    //check first name
+    if (typeof name !== 'string') {
       passedValidation = false;
-      validationMsg.password += '❌8 to 12 characters';
-    }
-    if (!/[a-z]/.test(password)) {
+      validationMsg.name = 'You must specify a first name';
+    } else if (name.trim().length === 0) {
       passedValidation = false;
-      validationMsg.password += '❌at least one lowercase letter';
+      validationMsg.name = 'First name is required';
     }
-    if (!/[A-Z]/.test(password)) {
+
+    //check email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (typeof email !== 'string') {
       passedValidation = false;
-      validationMsg.password += '❌at least one uppercase letter';
-    }
-    if (!/\d/.test(password)) {
+      validationMsg.email = 'You must specify an email';
+    } else if (email.trim().length === 0) {
       passedValidation = false;
-      validationMsg.password += '❌at least one number';
-    }
-    if (!/[!@#$%^&*()_+}{":;'?/>.<,]/.test(password)) {
+      validationMsg.email = 'Email is required';
+    } else if (!emailRegex.test(email)) {
       passedValidation = false;
-      validationMsg.password += '❌at least one special character';
+      validationMsg.email = 'Email is invalid';
     }
-  }
 
-  if (passedValidation) {
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      passwordConfirm: req.body.passwordConfirm,
-      // role: req.body.role,
-    });
+    //Check password
+    if (typeof password !== 'string') {
+      passedValidation = false;
+      validationMsg.password = 'Password is required';
+    } else if (password.trim().length === 0) {
+      passedValidation = false;
+      validationMsg.password = 'Password is required';
+    } else {
+      validationMsg.password = '';
+      if (password.length < 8 || password.length > 12) {
+        passedValidation = false;
+        validationMsg.password += '❌8 to 12 characters';
+      }
+      if (!/[a-z]/.test(password)) {
+        passedValidation = false;
+        validationMsg.password += '❌at least one lowercase letter';
+      }
+      if (!/[A-Z]/.test(password)) {
+        passedValidation = false;
+        validationMsg.password += '❌at least one uppercase letter';
+      }
+      if (!/\d/.test(password)) {
+        passedValidation = false;
+        validationMsg.password += '❌at least one number';
+      }
+      if (!/[!@#$%^&*()_+}{":;'?/>.<,]/.test(password)) {
+        passedValidation = false;
+        validationMsg.password += '❌at least one special character';
+      }
+    }
 
-    newUser
-      .save()
-      .then((userSaved) => {
-        console.log(`User ${userSaved.name} has been saved to the database`);
-
-        const msg = {
-          //to: "nickroma.seneca@gmail.com", // Send confirmation email to the user's email
-          subject: 'Registration Form Submission',
-          html: `Welcome to BambooTravel🎍 <br>
-          Customer's Full Name: ${name}<br>
-          Customer's Email Address: ${email}<br><br>
-          -------- Minh Thu (Tessa) Lu--------<br>`,
-        };
-
-        sendEmail({
-          email: userSaved.email,
-          subject: 'Registration Form Submission',
-          msg,
-        })
-          .then(() => {
-            createSendTokenAndRender(newUser, 201, res, '/');
-          })
-          .catch((err) => {
-            console.log(err);
-            res.render('general/signup', { values, validationMsg });
-          });
-      })
-      .catch((err) => {
-        errors.push("Couldn't save the user to the database" + err);
-        console.log(errors[0]);
-        res.render('general/signup', { values, validationMsg, errors });
+    if (passedValidation) {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+        // role: req.body.role,
       });
-  } else {
-    // Render the sign-up page with validation errors
-    res.render('general/signup', { values, validationMsg, errors });
+
+      newUser
+        .save()
+        .then((userSaved) => {
+          console.log(`User ${userSaved.name} has been saved to the database`);
+
+          const msg = {
+            //to: "nickroma.seneca@gmail.com", // Send confirmation email to the user's email
+            subject: 'Registration Form Submission',
+            html: `Welcome to BambooTravel🎍 <br>
+        Customer's Full Name: ${name}<br>
+        Customer's Email Address: ${email}<br><br>
+        -------- Minh Thu (Tessa) Lu--------<br>`,
+          };
+
+          sendEmail({
+            email: userSaved.email,
+            subject: 'Registration Form Submission',
+            msg,
+          })
+            .then(() => {
+              createSendTokenAndRender(newUser, 201, res, '/');
+            })
+            .catch((err) => {
+              console.log(err);
+              res.render('general/signup', { values, validationMsg });
+            });
+        })
+        .catch((err) => {
+          errors.push("Couldn't save the user to the database" + err);
+          console.log(errors[0]);
+          res.render('general/signup', { values, validationMsg, errors });
+        });
+    } else {
+      // Render the sign-up page with validation errors
+      res.render('general/signup', { values, validationMsg, errors });
+    }
+  } catch (err) {
+    res.status(400).render('general/error');
   }
 };
