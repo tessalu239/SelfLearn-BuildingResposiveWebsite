418b197101860d8aaf9efbbfd3f734f1752a5fa2
mtlu2 <minhthulu2399@gmail.com>
2024-09-15 21:45:48 -0400

    hero&navbar added

---
+++ Bootstrap Practice Project/yavin-website/js/script.js
@@ -0,0 +1,12 @@
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("navbar-sticky");
+    } else {
+      navbar.classList.remove("navbar-sticky");
+    }
+  });
+}
+document.addEventListener("DOMContentLoaded", userScroll);
