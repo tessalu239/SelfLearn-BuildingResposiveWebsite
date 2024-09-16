b33c26821555dc836b87443b8bd3df153e7ee2b3
mtlu2 <minhthulu2399@gmail.com>
2024-09-15 22:15:22 -0400

    stats section added

--- Bootstrap Practice Project/yavin-website/js/script.js
+++ Bootstrap Practice Project/yavin-website/js/script.js
@@ -9,4 +9,32 @@ function userScroll() {
     }
   });
 }
+
+function increaseStats() {
+  //Select all class counter
+  const counters = document.querySelectorAll(".counter");
+
+  //Loop: set each to 0
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+
+    //which has variable target = whatever in data-target, c= whatever counter currently, increment= target/200. If c<target->counter text= roundup (c+increment), time to do, 1. else c text=target
+    const updateCounter = () => {
+      const target = +counter.getAttribute("data-target");
+      const c = +counter.innerText;
+
+      const increment = target / 200;
+
+      if (c < target) {
+        counter.innerText = Math.ceil(c + increment);
+        setTimeout(updateCounter, 5);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    //call updateCounter()
+    updateCounter();
+  });
+}
 document.addEventListener("DOMContentLoaded", userScroll);
+document.addEventListener("DOMContentLoaded", increaseStats);
