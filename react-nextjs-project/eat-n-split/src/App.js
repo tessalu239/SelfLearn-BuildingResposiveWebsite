0390a5535b5507d2afd33db0fbc6edd5332131eb
mtlu2 <147965575+tessalu239@users.noreply.github.com>
2024-10-06 18:31:15 -0400

    update Split N Eat

---
+++ react-nextjs-project/eat-n-split/src/App.js
@@ -0,0 +1,160 @@
+import { useState } from "react";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  const [friendList, setFriendList] = useState(initialFriends);
+  const [show, setShow] = useState(false);
+  const [selected, setSelected] = useState(null);
+  function handleShow() {
+    setShow((show) => !show);
+  }
+  function handleAddFriend(newFriend) {
+    setFriendList((current) => [...current, newFriend]);
+    setShow(!show);
+  }
+  console.log(friendList);
+  function handleSelect(friend) {
+    if (selected === null) {
+      setSelected(friend);
+    } else {
+      if (selected === friend) setSelected(null);
+      else {
+        setSelected(friend);
+      }
+    }
+  }
+  console.log(selected);
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList
+          friendList={friendList}
+          handleSelect={handleSelect}
+          selected={selected}
+        />
+        {show && (
+          <FormAddFriend
+            friendList={friendList}
+            handleAddFriend={handleAddFriend}
+          />
+        )}
+        <Button onClick={handleShow}>{!show ? "Add friend" : "Close"}</Button>
+      </div>
+      <div>
+        {selected && (
+          <FormSplitBill friendList={friendList} selected={selected} />
+        )}
+      </div>
+    </div>
+  );
+}
+function FriendList({ friendList, handleSelect, selected }) {
+  return (
+    <ul>
+      {friendList.map((friend) => (
+        <Friend
+          key={friend.id}
+          friend={friend}
+          handleSelect={handleSelect}
+          selected={selected}
+        />
+      ))}
+    </ul>
+  );
+}
+function Friend({ friend, handleSelect, selected }) {
+  const isSelected = selected ? friend.id === selected.id : false;
+
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} /> <h2>{friend.name}</h2>
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${friend.balance}
+        </p>
+      )}
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${friend.balance * -1}
+        </p>
+      )}
+      <Button onClick={() => handleSelect(friend)}>
+        {isSelected ? "close" : "Select"}
+      </Button>
+    </li>
+  );
+}
+function FormAddFriend({ handleAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/150");
+  function handleSubmit(e) {
+    e.preventDefault();
+    const idFriend = crypto.randomUUID();
+    const newFriend = {
+      name,
+      image: `https://i.pravatar.cc/150?u=${idFriend}`,
+      balance: 0,
+      id: idFriend,
+    };
+    console.log(newFriend);
+    handleAddFriend(newFriend);
+  }
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑Friend name</label>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <label>🌆 Image URL</label>
+      <input type="text" onChange={(e) => setImage(e.target.value)} />
+      <Button onClick={(e) => handleSubmit(e)}>Add</Button>
+    </form>
+  );
+}
+function FormSplitBill({ selected }) {
+  return (
+    <form className="form-split-bill">
+      <h2>split a bill with {selected.name}</h2>
+      <label>💰 Bill Value</label>
+      <input type="number" />
+      <label>💁🏻‍♀️ Your expense</label>
+      <input type="number" />
+      <label>💁🏻 {selected.name}'s expense</label>
+      <input type="number" disable value={0} />
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option>You</option>
+        <option>{selected.name}</option>
+      </select>
+      <Button>Split Bill</Button>
+    </form>
+  );
+}
+
+// REUSABLE COMPONENTS
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
--- react-nextjs-project/steps/src/App.js
+++ react-nextjs-project/steps/src/App.js
@@ -28,24 +28,47 @@ export default function App() {
             <div className={step >= 2 && "active"}>2</div>
             <div className={step >= 3 && "active"}>3</div>
           </div>
-
-          <page className="message">{messages[step - 1]}</page>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
-            <button
+            <Button bgColor={"#7950f2"} textColor={"#fff"} onClick={handlePrev}>
+              <span>👈</span>Previous
+            </Button>
+            <Button bgColor={"#7950f2"} textColor={"#fff"} onClick={handleNext}>
+              Next<span>👉</span>
+            </Button>
+            {/* <button
               style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrev}
             >
               Previous
-            </button>
-            <button
+            </button> */}
+            {/* <button
               style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handleNext}
             >
               Next
-            </button>
+            </button> */}
           </div>
         </div>
       )}
     </>
+  );
+}
+function Button({ bgColor, textColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: "#7950f2", color: "#fff" }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <p>Step {step}:</p>
+      <span>{children}</span>
+    </div>
   );
 }
